6fed0e92aa95a0ba48e350c0acaa5f312076fbe1
Wesley Jose Santos <binary_quantum@outlook.com>
2023-07-29 06:33:48 -0300

    feat: add try catch to return http exception

--- lib/infra/interfaces/http_client_impl.dart
+++ lib/infra/interfaces/http_client_impl.dart
@@ -9,30 +9,38 @@ class HttpClientImpl implements HttpClientBase {
   @override
   Future<Either<HttpError, String>> get(String url,
       {Map<String, String>? headers, int timeout = 5000}) async {
-    final uri = Uri.parse(url);
-    var response = await http.get(uri, headers: headers)
-      .timeout(Duration(milliseconds: timeout));
-    if(response.statusCode >= HttpStatus.badRequest){
-      return Left(HttpResponseError(response.statusCode, response.body));
-    } else {
-      return Right(response.body);
+    try {
+      final uri = Uri.parse(url);
+      var response = await http.get(uri, headers: headers)
+        .timeout(Duration(milliseconds: timeout));
+      if(response.statusCode >= HttpStatus.badRequest){
+        return Left(HttpResponseError(response.statusCode, response.body));
+      } else {
+        return Right(response.body);
+      }
+    } catch (e) {
+      return Left(HttpRequestException(e));
     }
   }
 
   @override
   Future<Either<HttpError, String>> post(String url, String data,
       {ContentType? contentType, Map<String, String>? headers, int timeout = 5000}) async {
-    final uri = Uri.parse(url);
+    try {
+      final uri = Uri.parse(url);
 
-    headers ??= <String, String>{};
-    headers[HttpHeaders.contentTypeHeader] = contentType?.value ?? ContentType.json.value;
+      headers ??= <String, String>{};
+      headers[HttpHeaders.contentTypeHeader] = contentType?.value ?? ContentType.json.value;
 
-    var response = await http.post(uri, body: data, headers: headers)
-      .timeout(Duration(milliseconds: timeout));
-    if(response.statusCode >= HttpStatus.badRequest){
-      return Left(HttpResponseError(response.statusCode, response.body));
-    } else {
-      return Right(response.body);
+      var response = await http.post(uri, body: data, headers: headers)
+        .timeout(Duration(milliseconds: timeout));
+      if(response.statusCode >= HttpStatus.badRequest){
+        return Left(HttpResponseError(response.statusCode, response.body));
+      } else {
+        return Right(response.body);
+      }
+    } catch (e) {
+      return Left(HttpRequestException(e));
     }
   }
 }
